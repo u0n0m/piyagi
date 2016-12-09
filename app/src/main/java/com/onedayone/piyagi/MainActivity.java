8d316804222bcc197d78be94077fe477ec5b0acd
skh1 <u0n0m@naver.com>
2016-12-09 10:29:19 +0900

    파일에 반복시간 값 저장기능 추가

--- app/src/main/java/com/onedayone/piyagi/MainActivity.java
+++ app/src/main/java/com/onedayone/piyagi/MainActivity.java
@@ -1,18 +1,32 @@
 package com.onedayone.piyagi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.onedayone.piyagi.R;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         startActivity(new Intent(this, SplashActivity.class));
+
+//        try {
+//            Thread.sleep(4000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -25,5 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/onedayone/piyagi/RepeatActivity.java
+++ app/src/main/java/com/onedayone/piyagi/RepeatActivity.java
@@ -1,14 +1,22 @@
 package com.onedayone.piyagi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.onedayone.piyagi.R;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class RepeatActivity extends AppCompatActivity {
+    static final String FILE_NAME = "exam.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +40,42 @@ public class RepeatActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageButton btn_save = (ImageButton) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), HospitalActivity.class);
+//                startActivity(intent);
+                save_repeat_time();
+            }
+        });
     }
+
+    public boolean save_repeat_time(){
+        TextView repeat_hour = (TextView) findViewById(R.id.repeat_hour);
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            String str = repeat_hour.getText().toString();
+            fos.write(str.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            Log.e("File", "에러=" + e);
+        }
+
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            String str = new String(buffer);
+            //edit2.setText(str);
+            Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
+            toast.show();
+            fis.close();
+        } catch (Exception e) {
+            Log.e("File", "에러=" + e);
+        }
+        return true;
+    }
+
 }
