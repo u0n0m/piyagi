1c911ba1fee6f23ae0a38c90a376d645d6e515ad
skh1 <u0n0m@naver.com>
2016-12-12 10:25:53 +0900

    이것저것 테스트 하다보니 완전  지저분함.... 정리되면 이 커밋 버전은 지우자

--- app/src/main/java/com/onedayone/piyagi/RepeatService.java
+++ app/src/main/java/com/onedayone/piyagi/RepeatService.java
@@ -6,15 +6,19 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Shader;
 import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RepeatService extends Service {
     NotificationManager Notifi_M;
     ServiceThread thread;
     Notification Notifi ;
+    private TimerTask mTask;
+    private Timer mTimer;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -23,51 +27,58 @@ public class RepeatService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Notifi_M = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        RepeatServiceHandler handler = new RepeatServiceHandler();
-        thread = new ServiceThread(handler);
-        thread.start();
+        //Notifi_M = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //RepeatServiceHandler handler = new RepeatServiceHandler();
+        //thread = new ServiceThread(handler);
+        //thread.start();
+        Toast.makeText(getApplicationContext(),"RepeatService.class 시작 되었습니다!",Toast.LENGTH_SHORT).show();
+        mTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(RepeatService.this, AlarmActivity.class);
+                startActivity(intent);
+                //Toast.makeText(getApplicationContext(), "복약하세요", Toast.LENGTH_LONG).show();
+            }
+        };
+        mTimer = new Timer(true);
+        mTimer.schedule(mTask, 1000, 9000);
         return START_STICKY;
     }
 
     //서비스가 종료될 때 할 작업
     public void onDestroy() {
-        thread.stopForever();
-        thread = null;//쓰레기 값을 만들어서 빠르게 회수하라고 null을 넣어줌.
+        //thread.stopForever();
+        //thread = null;//쓰레기 값을 만들어서 빠르게 회수하라고 null을 넣어줌.
     }
 
-    class RepeatServiceHandler extends Handler {
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            Intent intent = new Intent(RepeatService.this, RepeatActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(RepeatService.this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
-
-            Notifi = new Notification.Builder(getApplicationContext())
-                    .setContentTitle("주기복용")
-                    .setContentText("정해진 시간마다 복약하세요")
-                    .setSmallIcon(R.drawable.piyagi1)
-                    .setTicker("알림!!!")
-                    .setContentIntent(pendingIntent)
-                    .build();
-
-            //소리추가
-            Notifi.defaults = Notification.DEFAULT_SOUND;
-
-            //알림 소리를 한번만 내도록
-            Notifi.flags = Notification.FLAG_ONLY_ALERT_ONCE;
-
-            //확인하면 자동으로 알림이 제거 되도록
-            Notifi.flags = Notification.FLAG_AUTO_CANCEL;
-
-            Notifi_M.notify( 777 , Notifi);
-
-            //토스트 띄우기
-            //Toast.makeText(RepeatService.this, "복약하세요", Toast.LENGTH_LONG).show();
-
-            Intent i = new Intent(getApplicationContext(), AlarmActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);
-
-        }
-    };
+//    class RepeatServiceHandler extends Handler {
+//        @Override
+//        public void handleMessage(android.os.Message msg) {
+//            Intent intent = new Intent(RepeatService.this, RepeatActivity.class);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(RepeatService.this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//            Notifi = new Notification.Builder(getApplicationContext())
+//                    .setContentTitle("주기복용")
+//                    .setContentText("정해진 시간마다 복약하세요")
+//                    .setSmallIcon(R.drawable.piyagi1)
+//                    .setTicker("알림!!!")
+//                    .setContentIntent(pendingIntent)
+//                    .build();
+//            //소리추가
+//            Notifi.defaults = Notification.DEFAULT_SOUND;
+//            //알림 소리를 한번만 내도록
+//            Notifi.flags = Notification.FLAG_ONLY_ALERT_ONCE;
+//            //확인하면 자동으로 알림이 제거 되도록
+//            Notifi.flags = Notification.FLAG_AUTO_CANCEL;
+//            Notifi_M.notify( 777 , Notifi);
+//
+//            //토스트 띄우기
+//            //Toast.makeText(RepeatService.this, "복약하세요", Toast.LENGTH_LONG).show();
+//
+//            //알람 화면 띄우는 Activity 실행시키기
+//            Intent i = new Intent(getApplicationContext(), AlarmActivity.class);
+//            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(i);
+//        }
+//    };
 }
