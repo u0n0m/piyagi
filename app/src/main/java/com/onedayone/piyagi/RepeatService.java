56fa2a2dd30b7a4f93701f6a436637b23e6c183a
skh1 <u0n0m@naver.com>
2016-12-19 15:13:10 +0900

    수정요청 사항 잔뜩 들어와서 왕창 변경함. - 스플래쉬 화면 수정함.. - 나머지는 이제부터 바꿔야함

--- app/src/main/java/com/onedayone/piyagi/RepeatService.java
+++ app/src/main/java/com/onedayone/piyagi/RepeatService.java
@@ -29,7 +29,7 @@ public class RepeatService extends Service  {
         Integer second = Integer.parseInt(intent.getStringExtra("total_minutes")) * 60 * 100; // 10분의 1 간격으로 작동,  알람작동 확인이 오래걸려서
         Toast.makeText(this, second.toString(), Toast.LENGTH_SHORT).show();
         //alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + second, pIntent);
-        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, second, second, pIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, second, second, pIntent);
         return super.onStartCommand(intent, flags, startId);
     }
 
